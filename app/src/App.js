bfa2b9ca3f28c45521d56428b392cf4d509f672f
Abhinav Meda <abhinavmeda@Abhinavs-Air.phub.net.cable.rogers.com>
2022-08-09 01:41:26 -0400

    added border colors to each pokemon and made some progress on the hover over feature

--- app/src/App.js
+++ app/src/App.js
@@ -2,22 +2,31 @@ import './App.css';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 
-const Item = ({ name, sprite }) => {
+const Item = ({ name, sprite, types, color}) => {
+  const groupClassName = `group border-2 border-${color} border-solid rounded`
+  console.log(color)
   return (
-    <div className='group border-2 border-gray-900 border-solid rounded-lg text-center'>
-      <div className="group-hover:text-red-600">
-      {name}
-      <section className="container max-w-screen-lg mx-auto">
-        <img className="mx-auto" src={sprite} alt={"dude"}/>
-      </section>
-      </div>
+    <div className={groupClassName}>
+        <div>
+          <div className='text-center'>
+            {name}
+          </div>
+          <section className="container max-w-screen-lg mx-auto group-hover:hidden">
+            <img className="mx-auto" src={sprite} alt={name}/>
+          </section>
+          <div className="invisible group-hover:visible border-solid rounded bg-orange-300">
+            {types.length === 2? "types: ":"type: "}{types.join('-')}
+          </div>
+        </div>
+        
+
     </div>
   )
 }
 const Grid = ({ data }) => {
   return (
     <div className="grid grid-cols-8 gap-4">
-      {data.map((items,i) => <Item key={i} name={items.name} sprite={items.sprite}/>)}
+      {data.map((items,i) => <Item key={i} name={items.name} color={items.color} sprite={items.sprite} types={items.type.map(each => each.type.name)}/>)}
   </div>
   )
 } 
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -4,8 +4,38 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue': '#267ba6',
+        'gray': '#b3b9bc',
+        'red': '#f02f2f',
+        'white':'#e0eff3',
+        'black': '#323536',
+        'brown': '#88561a',
+        'green': '#35cf37',
+        'pink' : '#db5ae8',
+        'purple': '#6b5ac6',
+        'yellow': '#f3ce15'
+      },
+    },
   },
   plugins: [],
+  variants: {
+    extend: {
+      visibility: ["group-hover"],
+    },
+   },
+   safelist: [
+    'border-blue',
+    'border-gray',
+    'border-red',
+    'border-white',
+    'border-black',
+    'border-brown',
+    'border-green',
+    'border-pink',
+    'border-yellow',
+    'border-purple'
+  ]
 }
 
